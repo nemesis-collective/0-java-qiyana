c8ac28b2f50cd14587190e6308ccba0c0f1f9f0e
qiyanatech <jp20171617@gmail.com>
2024-06-03 22:30:06 -0300

    Cleaner isValidName method.

--- src/main/java/com/nemesis/training/NameValidator.java
+++ src/main/java/com/nemesis/training/NameValidator.java
@@ -4,9 +4,8 @@ public class NameValidator {
   public static boolean isValidName(String name) {
     if (name.length() < 8 || name.length() > 25) {
       return false;
-    } else {
-      String validName = name.toLowerCase();
-      return validName.equals(name);
     }
+    String validName = name.toLowerCase();
+    return validName.equals(name);
   }
 }
