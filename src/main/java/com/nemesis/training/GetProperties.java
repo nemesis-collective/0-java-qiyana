dff01b9c98fd3e41c2ce283254a769ec36231608
qiyanatech <jp20171617@gmail.com>
2024-06-04 14:53:13 -0300

    Create GetProperties class.

---
+++ src/main/java/com/nemesis/training/GetProperties.java
@@ -0,0 +1,20 @@
+package com.nemesis.training;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import static jdk.internal.org.jline.utils.Log.debug;
+
+public class GetProperties {
+    public static Properties getProperties(String propertiesName) {
+        final Properties properties = new Properties();
+        try (InputStream inputStream =
+                     Thread.currentThread().getContextClassLoader().getResourceAsStream(propertiesName)) {
+            properties.load(inputStream);
+        } catch (Exception e) {
+            debug("Error in fetching properties: ", e);
+            return null;
+        }
+        return properties;
+    }
+}
