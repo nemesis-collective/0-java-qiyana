f783b02f8ec7e6936378bfc9eab65cefd1594c0f
qiyanatech <jp20171617@gmail.com>
2024-06-03 22:37:35 -0300

    Add getters and setters to the User class

--- src/main/java/com/nemesis/training/User.java
+++ src/main/java/com/nemesis/training/User.java
@@ -1,11 +1,26 @@
 package com.nemesis.training;
 
 public class User {
-  Long id;
-  String name;
+  private Long id;
+  private String name;
 
-  User(Long id, String name) {
+  User(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setId(Long id) {
     this.id = id;
+  }
+
+  public void setName(String name) {
     this.name = name;
   }
 }
