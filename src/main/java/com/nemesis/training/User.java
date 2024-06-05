32f7c16014b14be40fc60ef18932d66d7cacbe7d
qiyanatech <jp20171617@gmail.com>
2024-06-05 16:15:16 -0300

    Add Lombok to create Getters and Setters to User properties.

--- src/main/java/com/nemesis/training/User.java
+++ src/main/java/com/nemesis/training/User.java
@@ -1,26 +1,17 @@
 package com.nemesis.training;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class User {
+  @Getter @Setter
   private Long id;
+
+  @Getter @Setter
   private String name;
 
   User(String name) {
     this.name = name;
   }
 
-  public Long getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
 }
