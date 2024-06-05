99dcf4720a72ea7be032ce1a0ed2f5a1a9bf6564
qiyanatech <jp20171617@gmail.com>
2024-06-05 16:25:36 -0300

    Rename Properties to DBProperties.

--- src/main/java/com/nemesis/training/DBProperties.java
+++ src/main/java/com/nemesis/training/DBProperties.java
@@ -1,17 +1,17 @@
 package com.nemesis.training;
 
 import java.io.InputStream;
+import java.util.Properties;
 
-import static jdk.internal.org.jline.utils.Log.debug;
 
-public class Properties {
-  public static java.util.Properties getProperties(String propertiesName) {
-    final java.util.Properties properties = new java.util.Properties();
+public class DBProperties {
+  public static Properties getProperties(String propertiesName) {
+    final Properties properties = new Properties();
     try (InputStream inputStream =
         Thread.currentThread().getContextClassLoader().getResourceAsStream(propertiesName)) {
       properties.load(inputStream);
     } catch (Exception e) {
-      debug("Error in fetching properties: ", e);
+      System.out.println("Error in fetching properties: " + e);
       return null;
     }
     return properties;
