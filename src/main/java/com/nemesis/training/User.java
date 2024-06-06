9826cd891081c45e1434c34af262d3310760f672
qiyanatech <jp20171617@gmail.com>
2024-06-06 00:13:28 -0300

    Remove @Setter from class attributes.

--- src/main/java/com/nemesis/training/User.java
+++ src/main/java/com/nemesis/training/User.java
@@ -1,13 +1,12 @@
 package com.nemesis.training;
 
 import lombok.Getter;
-import lombok.Setter;
 
+@Getter
 public class User {
-  @Getter @Setter
+
   private Long id;
 
-  @Getter @Setter
   private String name;
 
   User(String name) {
