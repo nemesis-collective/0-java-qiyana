8900be317ec9032d33bebef9a53f40bdd3848600
qiyanatech <jp20171617@gmail.com>
2024-06-06 00:42:39 -0300

    NameValidator also checks if the name has symbols or numbers.

--- src/main/java/com/nemesis/training/NameValidator.java
+++ src/main/java/com/nemesis/training/NameValidator.java
@@ -1,11 +1,20 @@
 package com.nemesis.training;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NameValidator {
   public static boolean isValidName(String name) {
     if (name.length() < 8 || name.length() > 25) {
       return false;
     }
-    String validName = name.toLowerCase();
-    return validName.equals(name);
+
+    String REGEX = ".*[0-9A-Z\\W_].*";
+
+    Pattern pattern = Pattern.compile(REGEX);
+
+    Matcher matcher = pattern.matcher(name);
+
+    return !matcher.matches();
   }
 }
