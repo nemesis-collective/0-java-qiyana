3be3643fc1150b24d3e56c34fd867cb6bf39a2b8
qiyanatech <jp20171617@gmail.com>
2024-06-15 23:23:10 -0300

    Create test.application.properties.

--- src/test/java/com/nemesis/training/UsersStoreTest.java
+++ src/test/java/com/nemesis/training/UsersStoreTest.java
@@ -25,22 +25,6 @@ public class UsersStoreTest {
         verify(statementMock).execute(createTableSQL);
     }
 
-
-//    @Test
-//    void addUserTest_shouldCreate() throws SQLException {
-//        String insertSQL = "INSERT INTO USERS (name) VALUES (?)";
-//        String name = "joaopaulo";
-//        Connection connMock = mock(Connection.class);
-//        PreparedStatement statementMock = mock(PreparedStatement.class);
-//
-//        when(connMock.prepareStatement(insertSQL, Statement.RETURN_GENERATED_KEYS)).thenReturn(statementMock);
-//
-//        when(statementMock.setString(1,any())).thenThrow(new SQLException());
-//
-//
-//        new UsersStore(connMock);
-//        verify(statementMock).execute(insertSQL);
-//    }
     @Test
     void addUserTest_shouldAddUser() throws SQLException, IOException {
         String nome = "joaopaulo";
@@ -48,22 +32,6 @@ public class UsersStoreTest {
         UsersStore usersStore = new UsersStore(conn);
         usersStore.addUser(nome);
     }
-
-//    @Test
-//    public void getConnectionTest_shouldReturnSQLException() throws SQLException, IOException {
-//
-//        UsersStore usersStoreMock = mock(UsersStore.class);
-//        // Connection conn = mock(Connection.class);
-//
-//        when(usersStoreMock.getConnection()).thenThrow(new SQLException());
-//
-//        SQLException exception = assertThrows(SQLException.class, () -> {
-//            UsersStore.getConnection();
-//        });
-//
-//        assertEquals("An error occurred while connecting to database.", exception.getMessage());
-//
-//    }
 
     @Test
     public void getConnectionTest_shouldReturnConnection() throws SQLException, IOException {
