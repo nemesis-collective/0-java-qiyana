5a2d88317a14ed250afc8e254587160b9da43dbf
qiyanatech <jp20171617@gmail.com>
2024-06-15 23:22:57 -0300

    Create ConfigTest.

---
+++ src/test/java/com/nemesis/training/ConfigTest.java
@@ -0,0 +1,39 @@
+package com.nemesis.training;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConfigTest {
+    static Config config;
+    @BeforeAll
+    static void setUp() {
+        config = Config.getConfig();
+    }
+
+    @Test
+    void getPropertiesTest_whenParamsValid_shouldReturnProperties() throws IOException {
+        Properties properties = config.getProperties("test.application.properties");
+        assertEquals("testuser", properties.getProperty("db.username"));
+        assertEquals("testpass", properties.getProperty("db.password"));
+    }
+
+    @Test
+    void getPropertiesTest_whenParamsInvalid_shouldReturnIOException() throws IOException{
+
+        IOException exception = assertThrows(IOException.class, () -> {
+            config.getProperties("non-existent.properties");
+        });
+
+        assertEquals("Property file 'non-existent.properties' was not found or could not be read.", exception.getMessage());
+    }
+
+
+}
+
