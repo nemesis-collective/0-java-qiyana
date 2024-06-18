e649f179866c3daf6a8737ff1ad07f99d3c29bec
qiyanatech <jp20171617@gmail.com>
2024-06-17 21:08:07 -0300

    Verify if the id of the User object created is 0

--- src/main/java/com/nemesis/training/Main.java
+++ src/main/java/com/nemesis/training/Main.java
@@ -4,21 +4,27 @@ import java.sql.Connection;
 import java.util.Properties;
 
 public class Main {
-  public static void main(String[] args) {
-    try {
-      String username = args[0];
-      if (!NameValidator.isValidName(username)) {
-        throw new Exception(
-            "Please, write a name with 8 to 25 characters without capital letters or symbols.");
-      }
-      Connection conn = UsersStore.getConnection("application.properties");
-      UsersStore usersStore = new UsersStore(conn);
-      usersStore.createUser(username);
+    public static void main(String[] args) {
+        try {
+            String username = args[0];
+            if (!NameValidator.isValidName(username)) {
+                throw new Exception(
+                        "Please, write a name with 8 to 25 characters without capital letters or symbols.");
+            }
+            Connection conn = UsersStore.getConnection("application.properties");
+            UsersStore usersStore = new UsersStore(conn);
+            User user = usersStore.createUser(username);
 
-    } catch (ArrayIndexOutOfBoundsException e) {
-      System.out.println("No names were written.");
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+            if (user.getId() == 0) {
+                System.err.println("Failed to save name to database.");
+            } else {
+                System.out.println("Name added successfully with ID = " + user.getId());
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("No names were written.");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 }
