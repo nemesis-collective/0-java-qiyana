1db9ac290867fc15c686d32f1885761dd02811a6
qiyanatech <jp20171617@gmail.com>
2024-06-25 13:18:09 -0300

    Change getConnection to catch exceptions

--- src/main/java/com/nemesis/training/Main.java
+++ src/main/java/com/nemesis/training/Main.java
@@ -6,24 +6,21 @@ public class Main {
   public static void main(String[] args) {
     try {
       String username = args[0];
-      if (!NameValidator.isValidName(username)) {
-        throw new Exception(
+      if (NameValidator.isValidName(username)) {
+        Connection conn = UsersStore.getConnection("application.properties");
+        UsersStore usersStore = new UsersStore(conn);
+        User user = usersStore.createUser(username);
+        if (user.getId() == 0) {
+          System.err.println("Failed to save name to database.");
+        } else {
+          System.out.println("Name added successfully with ID = " + user.getId());
+        }
+      } else {
+        System.out.println(
             "Please, write a name with 8 to 25 characters without capital letters or symbols.");
       }
-      Connection conn = UsersStore.getConnection("application.properties");
-      UsersStore usersStore = new UsersStore(conn);
-      User user = usersStore.createUser(username);
-
-      if (user.getId() == 0) {
-        System.err.println("Failed to save name to database.");
-      } else {
-        System.out.println("Name added successfully with ID = " + user.getId());
-      }
-
     } catch (ArrayIndexOutOfBoundsException e) {
       System.out.println("No names were written.");
-    } catch (Exception e) {
-      throw new RuntimeException(e);
     }
   }
 }
