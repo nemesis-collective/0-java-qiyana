b48547588b54fe9bb8fb8e389df3db91f446800e
qiyanatech <jp20171617@gmail.com>
2024-06-25 20:12:12 -0300

    Change Main to allow tests

--- src/main/java/com/nemesis/training/Main.java
+++ src/main/java/com/nemesis/training/Main.java
@@ -4,23 +4,32 @@ import java.sql.Connection;
 
 public class Main {
   public static void main(String[] args) {
+    Main main = new Main();
+    main.run(args);
+  }
+
+  public void run(String[] args) {
     try {
       String username = args[0];
       if (NameValidator.isValidName(username)) {
         Connection conn = UsersStore.getConnection("application.properties");
         UsersStore usersStore = new UsersStore(conn);
         User user = usersStore.createUser(username);
-        if (user.getId() == 0) {
-          System.err.println("Failed to save name to database.");
-        } else {
-          System.out.println("Name added successfully with ID = " + user.getId());
-        }
+        UserCreation(user);
       } else {
         System.out.println(
             "Please, write a name with 8 to 25 characters without capital letters or symbols.");
       }
     } catch (ArrayIndexOutOfBoundsException e) {
-      System.out.println("No names were written.");
+      System.out.print("No names were written.");
+    }
+  }
+
+  public void UserCreation(User user) {
+    if (user.getId() == 0) {
+      System.err.print("Failed to save name to database.");
+    } else {
+      System.out.println("Name added successfully with ID = " + user.getId());
     }
   }
 }
