6c77f428f6617cfda7a4947a738d807f93fea6c9
qiyanatech <jp20171617@gmail.com>
2024-06-26 18:52:11 -0300

    Resume three test to one parameterized test

--- src/test/java/com/nemesis/training/NameValidatorTest.java
+++ src/test/java/com/nemesis/training/NameValidatorTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class NameValidatorTest {
 
@@ -13,21 +15,10 @@ public class NameValidatorTest {
     assertTrue(response);
   }
 
-  @Test
-  void NameValidatorTest_whenNameIsInvalid_mustReturnFalse() {
-    var response = NameValidator.isValidName("João123");
-    assertFalse(response);
-  }
-
-  @Test
-  void NameValidatorTest_whenNameIsShort_mustReturnFalse() {
-    var response = NameValidator.isValidName("joao");
-    assertFalse(response);
-  }
-
-  @Test
-  void NameValidatorTest_whenNameIsLong_mustReturnFalse() {
-    var response = NameValidator.isValidName("abcdefghijklmnopqrstuvwxyz");
+  @ParameterizedTest
+  @ValueSource(strings = {"João123", "joao", "abcdefghijklmnopqrstuvwxyz"})
+  void NameValidatorTest_whenNameIsInvalid_mustReturnFalse(String name) {
+    var response = NameValidator.isValidName(name);
     assertFalse(response);
   }
 }
